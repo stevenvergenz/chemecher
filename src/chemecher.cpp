2c1f6797cc842c7522ac6e6f3baa46b3b843ebe8
Steven Vergenz <vergenzs@gmail.com>
2010-12-13 18:33:22 +0000

    Modifications for non-interactive mode begun, minimal changes so far.

--- src/chemecher.cpp
+++ src/chemecher.cpp
@@ -1,16 +1,34 @@
 #include <QApplication>
 #include <QDebug>
 #include <iostream>
-
+#include <cstring>
 #include "mainwindow.h"
 
 int main(int argc, char** argv)
 {
-	QApplication app(argc, argv);
-	app.setApplicationName("Chemecher 4.0");
-
-	MainWindow ui;
-	ui.show();
-
-	return app.exec();
+	bool isCLI = false;
+	
+	// parse over the arguments for the initialization param
+	for(int i=0; i<argc; i++){
+		if( strcmp(argv[i], "--non-interactive")==0 ){
+			isCLI = true;
+			break;
+		}
+	}
+	
+	if( isCLI ){
+		// non-interactive flag is set, don't launch GUI
+		
+	}
+	else {
+		// normal behavior: launch GUI
+		QApplication app(argc, argv);
+		app.setApplicationName("Chemecher");
+		app.setApplicationVersion("4.0.0");
+	
+		MainWindow ui;
+		ui.show();
+		return app.exec();
+	}
 }
+
